a5dd2ad436a8f8c189006423bb8c15d3348520cf
Mohit Panchariya <iammohitpanchariya@gmail.com>
2024-06-18 22:17:08 +0530

    Add two character tokens
    The following two character tokens have been added:
    1. ==, EQ
    2. !=, NOT_EQ

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -39,7 +39,14 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		// two character equality token
+		if l.peakChar() == '=' {
+			ch := l.ch   // store the current character
+			l.readChar() // advance the lexer
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -65,7 +72,14 @@ func (l *Lexer) NextToken() token.Token {
 	case '>':
 		tok = newToken(token.GT, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		// two character inequality token
+		if l.peakChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -116,4 +130,12 @@ func (l *Lexer) readNumber() string {
 		l.readChar()
 	}
 	return l.input[position:l.position]
+}
+
+func (l *Lexer) peakChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
