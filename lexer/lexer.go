9c9547335bfce64ea58c31fb952fd21d41a1ddb9
Mohit Panchariya <iammohitpanchariya@gmail.com>
2024-06-18 07:45:23 +0530

    Tokenize identifiers, keywords, numbers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -35,6 +35,8 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitesapace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -55,8 +57,51 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok // early return as next character has already been read/lexer already points to next character
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	// read the next character
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) skipWhitesapace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func isLetter(ch byte) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '_'
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position // first position where a letter is found
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position] // l.position now points to a non-letter index
+}
+
+func isDigit(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
 }
