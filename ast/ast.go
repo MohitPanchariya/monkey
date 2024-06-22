c45e1aa9d67e01e1aa763c03fdf573d6e29ec44b
Mohit Panchariya <iammohitpanchariya@gmail.com>
2024-06-22 11:32:42 +0530

    Define AST structure(Node,Statement,Expression)
    The Node interface is to be "implemented" by all types of nodes in
    the AST.
    
    Nodes in the AST which are statements(e.g. a  let statement) and nodes
    in the AST which are expressions(5 * 5 * total) are to "implement" the
    statement and expression interface respectively.
    
    The Program struct represents the root node of the AST. It also stores
    all the statements in the program.
    
    The LetStatement struct represents a let statement and "implements" the
    statement interface. It points to an identifier, a value which is to be
    bound to that identifier and also stores its own token.
    
    The identifier struct represents an identifier. It stores its own token
    and also the value associated with this identifier.

---
+++ ast/ast.go
@@ -0,0 +1,54 @@
+package ast
+
+import "github.com/MohitPanchariya/monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode() // dummy method to throw error when a Statement is used instead of an Expression
+}
+
+type Expression interface {
+	Node
+	expressionNode() // dummy method to throw error when an Expression is used instead of a Statement
+}
+
+// Program node represents the root node of the AST.
+// Each monkey program is a series of statements
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // Every node in the tree keeps track of the token its associated with
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string      // Identifiers can be value producing as well (e.g. let x = valueProducingIdentifier)
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+func (i *Identifier) expressionNode() {}
