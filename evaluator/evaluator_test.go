ea0a2724c5f0f30da0061c948a6ebee01d14518f
Mohit Panchariya <iammohitpanchariya@gmail.com>
2024-07-06 15:34:23 +0530

    Evaluate boolean literals

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -46,3 +46,34 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 
 	return false
 }
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evalated := testEval(tt.input)
+		testBooleanObject(t, evalated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t\n", result.Value, expected)
+		return false
+	}
+
+	return true
+}
