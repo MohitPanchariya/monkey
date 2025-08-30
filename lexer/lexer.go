45810221c802f8d41692d735a48bb1a9c83c4afe
Mohit Panchariya <iammohitpanchariya@gmail.com>
2025-08-30 17:31:31 +0530

    Extend tokens - identifiers,keywords,integers
    With the extended token set, the lexer now identifies,
    * identifiers - variable, and function names
    * keywords - the let, and fn keywords
    * integers - integer literals

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -32,9 +32,47 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
+// `skipWhiteSpace` skips over spaces, tabs, and new lines since
+// they hold significance in monkey.
+func (l *Lexer) skipWhiteSpace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+// `isLetter` returns true if the character is a letter.
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// `isDigit` returns true if the character is a digit.
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+// `readIdentifier` returns a parsed identifier.
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// `readNumber` returns a parsed number.
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
 // `NextToken` returns the next token in the source code.
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhiteSpace()
 
 	switch l.ch {
 	case '=':
@@ -56,6 +94,22 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		// The below early return is required since readIdentifier reads
+		// until it encounters a non letter character. Therefore, the lexer
+		// has been advanced to the start of the next token by readIdentifier
+		// and the readChar prior to exiting the function is not required.
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			return newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
