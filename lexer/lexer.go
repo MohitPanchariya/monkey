1922802a85d2751016b665f54da782b9b4550d4c
Mohit Panchariya <iammohitpanchariya@gmail.com>
2025-08-30 18:47:16 +0530

    Add equality operators
    The equality (==) and not equality (!=) operator tokens
    have been added.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -68,6 +68,15 @@ func (l *Lexer) readNumber() string {
 	return l.input[position:l.position]
 }
 
+// `peekChar` returns the next character without advancing the lexer.
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
 // `NextToken` returns the next token in the source code.
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
@@ -76,7 +85,12 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			tok = token.Token{Type: token.EQ, Literal: "=="}
+			l.readChar()
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -92,7 +106,12 @@ func (l *Lexer) NextToken() token.Token {
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '=' {
+			tok = token.Token{Type: token.NOT_EQ, Literal: "!="}
+			l.readChar()
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
 	case '/':
